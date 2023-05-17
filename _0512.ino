f6bc816c4e54e0748b46b2366ff8465d8e23660b
Nomura369 <122143728+Nomura369@users.noreply.github.com>
2023-05-17 15:11:58 +0800

    Update _0512.ino

--- _0512.ino
+++ _0512.ino
@@ -28,47 +28,47 @@ int noteDurations2[] = {
   8, 8, 8, 16, 16, 16, 8, 8, 8, 8, 8, 4
 };
 
+int melodyNumber = 0;
+bool previousState = 1, presentState = 1;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(6, INPUT_PULLUP);
-  Serial.begin(9600);
 }
-
-byte previousState = 1, presentState = 1, melodyNumber = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  presentState = digitalRead(5);
-  if(presentState == 0 && previousState == 1){
-    melodyNumber++;
-    if(melodyNumber > 1) melodyNumber = 0;      
-  }
-  Serial.println(melodyNumber);  
+  if(melodyNumber == 0) {
+    for (int thisNote = 0; thisNote < 23; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(8, melody[thisNote], noteDuration);
 
-  if(melodyNumber == 0){
-    for(int thisNote = 0; thisNote < 23; thisNote++){
-      int noteDuration;
-      noteDuration = 1000 / noteDurations[thisNote];    
-        tone(8, melody[thisNote], noteDuration);
-    
-        int pauseBetweenNotes = noteDuration * 1.3;
-        delay(pauseBetweenNotes);
-        noTone(8);
-        }
-    //delay(200);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(8); // stop the tone playing:
+      presentState = digitalRead(6);
+      if(presentState == 0 &&  previousState == 1) {
+        melodyNumber = melodyNumber + 1;
+        thisNote = 23;
+      }
+      previousState = presentState;
+    }
   }
+  if(melodyNumber == 1) {
+    for (int thisNote = 0; thisNote < 23; thisNote++) {
+      int noteDuration = 1000 / noteDurations2[thisNote];
+      tone(8, melody2[thisNote], noteDuration);
 
-  if(melodyNumber == 1){
-    for(int thisNote = 0; thisNote < 23; thisNote++){
-      int noteDuration;
-      noteDuration = 1000 / noteDurations2[thisNote];    
-        tone(8, melody2[thisNote], noteDuration);
-    
-        int pauseBetweenNotes = noteDuration * 1.3;
-        delay(pauseBetweenNotes);
-        noTone(8);
-        }
-    //delay(200);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(8); // stop the tone playing:
+      presentState = digitalRead(6);
+      if(presentState == 0 &&  previousState == 1)
+      {
+        melodyNumber = melodyNumber - 1;
+        thisNote = 23;
+      }
+      previousState = presentState;
+    }
   }
-  previousState = presentState;
 }
